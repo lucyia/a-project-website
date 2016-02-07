e3020314f34a6e9211c769a62116174c36889db7
lucyia <lucia.koc@gmail.com>
2016-02-07 23:47:28 +0100

    added js minify

--- devel/gulpfile.js
+++ devel/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 
 gulp.task('default', ['less', 'watch', 'scripts']);
 
@@ -13,16 +14,17 @@ gulp.task('watch', function(){
 gulp.task('less', function () {
   return gulp.src('./assets/less/style.less')
     .pipe(less())
-    .pipe(gulp.dest('../public'));
+    .pipe(gulp.dest('../public'))
 });
 
 gulp.task('scripts', function() {
 	return gulp.src([
-		'./bower_components/jquery/dist/jquery.min.js',
-		'./bower_components/bootstrap/dist/js/bootstrap.min.js',
+		'./bower_components/jquery/dist/jquery.js',
+		'./bower_components/bootstrap/dist/js/bootstrap.js',
 		'./bower_components/bootstrap/js/collapse.js',
 		'./assets/js/functions.js'
 		])
 	.pipe(concat('scripts.js'))
+	.pipe(minify())
 	.pipe(gulp.dest('../public'))
 });
